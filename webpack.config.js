d6f6ad5ff773f36b13069cb6f4617a7635712454
Monika-Soam17 <86161447+Monika-Soam17@users.noreply.github.com>
2022-04-05 16:29:55 +0530

    setup webpack and linters

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }, ],
+  },
+  devServer: {
+    static: `${__dirname}/dist/`,
+    port: 8080,
+  },
+};
